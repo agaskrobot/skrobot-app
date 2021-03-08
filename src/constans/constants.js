8b861c97c92c9cf7395d7f891f90afd665fe3889
agaskrobot <aga@solarnub.com>
2021-03-08 12:00:31 +0100

    conected with api

--- src/constans/constants.js
+++ src/constans/constants.js
@@ -1,8 +1,16 @@
+// Lotery dates
 export const DATES = [
   { label: '04-09-2020', value: '20200904' },
-  { label: '11-09-2020', value: '20200911' }
+  { label: '11-09-2020', value: '20200911' },
+  { label: '18-09-2020', value: '20200918' },
+  { label: '25-09-2020', value: '20200925' },
+  { label: '02-10-2020', value: '20201002' },
+  { label: '09-10-2020', value: '20201009' },
+  { label: '16-10-2020', value: '20201016' },
+  { label: '23-10-2020', value: '20201023' }
 ];
 
+// Info about tier and matches
 export const RANK_INFO = {
   rank1: { tier: 'I', match: '5 Numbers + 2 Euronumbers' },
   rank2: { tier: 'II', match: '5 Numbers + 1 Euronumber' },
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -4,51 +4,7 @@ import { getResultsInfo } from '../../api';
 import { Alert, Spinner } from '../../components';
 import { makeRows } from '../../helpers/makeRows';
 import { DATES } from '../../constans';
-
 import { Table, Dropdown, Section } from 'skrobot-lib';
-
-const RESULT_DEMO = {
-  last: [
-    {
-      nr: 442,
-      currency: 'EUR',
-      date: {
-        full: 'Die Lottozahlen vom Freitag, den 04.09.2020',
-        day: 4,
-        month: 9,
-        year: 2020,
-        hour: 21,
-        minute: 0,
-        dayOfWeek: 'Freitag'
-      },
-      closingDate: '04.09.2020, 19:00',
-      lateClosingDate: '04.09.2020, 20:10',
-      drawingDate: '04.09.2020, 21:00',
-      numbers: [5, 23, 28, 38, 49],
-      euroNumbers: [3, 9],
-      jackpot: '10',
-      marketingJackpot: '10',
-      specialMarketingJackpot: '10',
-      climbedSince: 12,
-      Winners: 885440,
-      odds: {
-        rank0: { winners: 0, specialPrize: 0, prize: 0 },
-        rank1: { winners: 0, specialPrize: 0, prize: 1000000000 },
-        rank2: { winners: 0, specialPrize: 0, prize: 0 },
-        rank3: { winners: 8, specialPrize: 0, prize: 8097190 },
-        rank8: { winners: 38793, specialPrize: 0, prize: 1720 },
-        rank9: { winners: 37462, specialPrize: 0, prize: 1720 },
-        rank10: { winners: 62027, specialPrize: 0, prize: 1490 },
-        rank4: { winners: 59, specialPrize: 0, prize: 365970 },
-        rank5: { winners: 829, specialPrize: 0, prize: 23440 },
-        rank6: { winners: 1357, specialPrize: 0, prize: 11130 },
-        rank11: { winners: 203155, specialPrize: 0, prize: 820 },
-        rank7: { winners: 2573, specialPrize: 0, prize: 5030 },
-        rank12: { winners: 539177, specialPrize: 0, prize: 760 }
-      }
-    }
-  ]
-};
 
 export function Home() {
   const [error, setError] = useState(null);
@@ -57,13 +13,19 @@ export function Home() {
   const [results, setResults] = useState([]);
 
   const getResults = () => {
-    let rows = makeRows(RESULT_DEMO.last[0].odds);
+    setLoading(true);
 
-    setResults(rows);
-    // getResultsInfo(selectedDate)
-    //   .then((response) => {
-    //   })
-    //   .catch((error) => setError(error.message));
+    // Call api with selected date
+    getResultsInfo(selectedDate)
+      .then((response) => {
+        let rows = makeRows(response.data.last[0].odds);
+        setResults(rows);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setResults([]);
+      })
+      .finally(() => setLoading(false));
   };
 
   // useEffect loads results foe selected date
@@ -82,7 +44,7 @@ export function Home() {
       ) : (
         <div className="flex flex-wrap md:px-10 flex-row text-xl justify-center text-sm font-light min-w-max w-screen">
           <Alert error={error} onClose={() => setError(null)} />
-          <div className="flex-col items-center justify-center">
+          <div className="flex-col items-center justify-center mt-10">
             <Section>
               <Dropdown value={selectedDate} onChange={setSelectedDate} options={DATES} />
             </Section>
